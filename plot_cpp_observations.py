851ccdd68db92576ec615a156be5d783b7188c09
Mahmudur <mahmudhera93@gmail.com>
2023-09-21 20:32:51 -0400

    changed plotting with different scale factors

--- plot_cpp_observations.py
+++ plot_cpp_observations.py
@@ -6,6 +6,9 @@ import sys
 plt.rcParams["font.family"] = "tahoma"
 plt.rcParams['font.size'] = 11
 
+#scale_factors = [0.0001, 0.001, 0.01, 0.1];
+scale_factors = [0.01, 0.1, 0.5, 0.95]
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print('File not given')
@@ -14,7 +17,7 @@ if __name__ == "__main__":
     fig, axs = plt.subplots(2, 2, sharex=True)
     a, b = min(df['size_v'].tolist()), max(df['size_v'].tolist())
 
-    for s, coord in zip([0.0001, 0.001, 0.01, 0.1], [ [0,0], [0,1], [1,0], [1,1] ]):
+    for s, coord in zip(scale_factors, [ [0,0], [0,1], [1,0], [1,1] ]):
         df2 = df[ (df['s'] == s) ]
         axs[coord[0],coord[1]].scatter(df2['size_v'], 100*(df2['cos_org']-df2['cos_fmh'])/df2['cos_org'], s=1, color='#0077b6')
         axs[coord[0],coord[1]].plot( [a, b], [0, 0], color='#03045e')
